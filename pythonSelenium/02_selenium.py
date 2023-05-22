e1ed4a462ff8f8d8e519413f3843ff2f81fe7037
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 13:54:00 -0500

    Update 02_selenium.py

--- pythonSelenium/02_selenium.py
+++ pythonSelenium/02_selenium.py
@@ -22,12 +22,12 @@ driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Rahul")
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
 
 # #Static Dropdown
-# dropdown = Select(driver.find_element(By.ID,"exampleFormControlSelect1"))
-# dropdown.select_by_visible_text("Female")
-# dropdown.select_by_index(0)
-# #dropdown.select_by_value()
-#
-#
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+dropdown.select_by_value()
+
+
 # driver.find_element(By.XPATH, "//input[@type='submit']").click()
 # message = driver.find_element(By.CLASS_NAME, "alert-success").text
 # print(message)
