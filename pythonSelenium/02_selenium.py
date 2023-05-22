2769f567fb1001bf4c11b747ed87f7018fdb8263
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 14:47:54 -0500

    Update 02_selenium.py

--- pythonSelenium/02_selenium.py
+++ pythonSelenium/02_selenium.py
@@ -19,19 +19,7 @@ driver.find_element(By.ID, "exampleCheck1").click()
 # Xpath -  //tagname[@attribute='value'] -> //input[@type='submit']
 # CSS -  tagname[attribute='value'] -> //input[@type='submit'],  #id, .classname
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Rahul")
-driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
-
-# #Static Dropdown
-dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
-dropdown.select_by_visible_text("Female")
-dropdown.select_by_index(0)
-# dropdown.select_by_value()
-
-
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)
 assert "Success" in message
-
-driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys("helloagain")
-driver.find_element(By.XPATH, "(//input[@type='text'])[3]").clear()
