40cd0dcac40d4972ce71a9e9e1387b313dfe96dc
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 19:52:46 -0500

    Update 04_dropdown.py

--- pythonSelenium/04_dropdown.py
+++ pythonSelenium/04_dropdown.py
@@ -1,6 +1,11 @@
 
 from datetime import time
 from selenium import webdriver
+import time
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 #chrome driver
 from selenium.webdriver.chrome.service import Service
@@ -8,10 +13,25 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-service_obj = Service("/Users/itsyuimoriispace/Documents/GitHub/Selenium-Webdriver-with-PYTHON-from-Scratch---Frameworks/chromedriver_mac64/chromedriver");
-driver = webdriver.Chrome(service = service_obj)
+from selenium.webdriver.chrome.options import Options
 
-driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-driver.find_element(By.ID, 'autosuggest')
-time.sleep(2)
+def main():
+    service = Service('/Users/itsyuimoriispace/Downloads/chromedriver_mac64/chromedriver')
+    driver = webdriver.Chrome(service=service)
+
+    driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+    driver.find_element(By.ID, 'autosuggest').send_keys('ca')
+    time.sleep(2)
+    countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+    print(len(countries))
+
+    for country in countries:
+
+
+
+if __name__ == '__main__':
+    main()
