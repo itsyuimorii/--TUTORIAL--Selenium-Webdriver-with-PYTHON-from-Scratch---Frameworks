6aff5a6d5a1ccde1a0d2c5e6943aed11ce27c4b1
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 19:01:56 -0500

    update selenium practices

---
+++ pythonSelenium/03_Locators_Extension.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+#chrome driver
+from selenium.webdriver.chrome.service import Service
+#-- Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+service_obj = Service("/Users/itsyuimoriispace/Documents/GitHub/Selenium-Webdriver-with-PYTHON-from-Scratch---Frameworks/chromedriver_mac64/chromedriver");
+driver = webdriver.Chrome(service = service_obj)
+
+driver.get("https://rahulshettyacademy.com/client")
+
+driver.find_element(By.LINK_TEXT, "Forgot password?").click()
+driver.find_element(By.XPATH, "//form/div[1]/input").send_keys("demo@gmail.com")
+
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(2) input").send_keys("Hello@1234")
+
+driver.find_element(By.CSS_SELECTOR,"#confirmPassword").send_keys("Hello@1234")
+#driver.find_element(By.XPATH,"//button[@type='submit']").click()
+
+driver.find_element(By.XPATH,"//button[text()='Save New Password']").click()
