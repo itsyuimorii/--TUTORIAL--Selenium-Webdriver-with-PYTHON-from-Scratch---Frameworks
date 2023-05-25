9bb202f7cdf56f5a766206b5d85f88617b1b9e2c
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-25 14:36:30 -0500

    SelFramework

---
+++ PythonSelFramework/tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+import time
+driver = None
+
+
+
+@pytest.fixture()
+def setup():
+    global driver
+    browser_name=request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(executable_path="C:\\Users\\ymorii\\Desktop\\work_folder\\github\\Selenium-Webdriver-with-PYTHON-from-Scratch---Frameworks\\chromedriver_mac64\\chromedriver")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path="C:\\Users\\ymorii\\Desktop\\work_folder\\github\\Selenium-Webdriver-with-PYTHON-from-Scratch---Frameworks\\chromedriver_mac64\\chromedriver")
+    elif browser_name == "IE":
+        print("IE driver")
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
+ 
