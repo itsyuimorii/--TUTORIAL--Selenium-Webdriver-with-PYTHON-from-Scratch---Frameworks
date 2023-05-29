7461e6e17f18142b89f33e94a75ad76ec987102b
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-29 08:04:35 -0500

    etree

--- Requests_and_LXML/fundamentals/app.py
+++ Requests_and_LXML/fundamentals/app.py
@@ -1,11 +1,9 @@
 from lxml import etree 
-
-tree= etree.parse("fundamentals/src/web_page.html")
-
-print(tree)
-
-
-
+ 
+tree= etree.parse("./fundamentals/src/web_page.html", etree.HTMLParser())
+print(etree.tostring(tree))
+ 
+ 
 # pip install pipenv
 # pipenv install pylint autopep8 --dev
 # pip install lxml
